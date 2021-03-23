555ab6a31fa81f4536cf62c0833ed15b9432b2a7
Hanzhi Yin <yinhanzhi@gmail.com>
2021-03-23 11:29:08 -0500

    configured eslint

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb-typescript',
+    'airbnb/hooks',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: [
+    'react',
+    '@typescript-eslint',
+    'jest',
+  ],
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+  },
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 export default function Home() {
   return (
     <p>Home Page Works!</p>
-  )
+  );
 }
