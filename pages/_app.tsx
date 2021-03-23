1a1a74a483167c04f9fa8220a6bb91f852d07923
Hanzhi Yin <yinhanzhi@gmail.com>
2021-03-23 11:37:11 -0500

    add font css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 /* eslint-disable react/jsx-props-no-spreading */
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
