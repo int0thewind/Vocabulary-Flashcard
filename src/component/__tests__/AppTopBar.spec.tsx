cc1a5d33a2cebb13043401a50855d6a2d319b7ce
Hanzhi Yin <yinhanzhi@gmail.com>
2021-03-30 10:13:42 -0500

    add app top bar testing framework

---
+++ src/component/__tests__/AppTopBar.spec.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+describe('AppTopBar has correct routings.', () => {
+  it('AppTopBar can route to correct endpoints when user is signed in.', () => {
+
+  });
+
+  it('AppTopBar can route to correct endpoints when user is signed out.', () => {
+
+  });
+});
+
+describe('AppTopBar renders correct info on desktop.', () => {
+  it('AppTopBar renderes correct buttons when user is signed in.', () => {
+
+  });
+
+  it('AppTopBar renderes correct buttons when user is signed out.', () => {
+
+  });
+});
+
+describe('AppTopBar renders correct info on mobile.', () => {
+  it('AppTopBar renderes correct buttons when user is signed in.', () => {
+
+  });
+
+  it('AppTopBar renderes correct buttons when user is signed out.', () => {
+
+  });
+});
+
+describe('AppTopBar renders correct info when viewport has changed.', () => {
+  it('AppTopBar properly closed drawers when switching from mobile to desktop.', () => {
+
+  });
+});
