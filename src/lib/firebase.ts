fdffac94a99db0f3751d8b0ff7f62eee94b2109c
Hanzhi Yin <yinhanzhi@gmail.com>
2021-03-31 22:49:27 -0500

    delete useless svg file

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -14,6 +14,8 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 
+// TODO: for local dev and testing, consider using local emulators.
+
 const firebaseConfig = process.env.NODE_ENV === 'production' ? {
   apiKey: 'AIzaSyA1idIe2_-3X4oL7Z6GV-QOyxVIlZib8MM',
   authDomain: 'vocabulary-flashcard-938b9.firebaseapp.com',
@@ -36,6 +38,13 @@ if (!firebase.apps.length) firebase.initializeApp(firebaseConfig);
 
 export const appAuth = firebase.auth();
 export const appFirestore = firebase.firestore();
+
+// Use emulator if app is not in production mode.
+// Emulator config: see ./firebase.json emulators property.
+// if (process.env.NODE_ENV !== 'production') {
+//   appAuth.useEmulator('http://localhost:9099');
+//   appFirestore.useEmulator('localhost', 8080);
+// }
 
 type UseFirebaseUserType = [firebase.User | null, boolean, firebase.auth.Error | null];
 
