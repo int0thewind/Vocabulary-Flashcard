a4d0ad37e378adc77e05f82b7ef71fb378be579a
Hanzhi Yin <yinhanzhi@gmail.com>
2021-04-13 21:27:29 -0500

    add word data structure

--- src/type/Vocabulary.ts
+++ src/type/Vocabulary.ts
@@ -1,18 +1,46 @@
-type AudioRecording = {
-  [key: string]: URL,
+type WordSource = 'MW' | 'Oxford' | 'manual';
+
+/**
+ * A word may have multiple audio recording
+ * indicaing different accent/gender.
+ */
+type WordAudio = {
+  accent: string,
+  gender: string,
+  audio: URL,
 };
 
+/**
+ * A word may have multiple definitions.
+ * A definition may have multiple sample sentences.
+ */
 type WordExplaination = {
   definition: string,
   example: string[],
 };
 
-export default interface Vocabulary {
-  word: string,
-  phoneticSymbol: string,
-  audio: AudioRecording,
-  explaination: WordExplaination
+export default interface Word {
+  /** The word literal. */
+  literal: string;
 
-  addedAt: Date,
-  tag: string[],
+  /** Where the word is added? */
+  source: WordSource;
+
+  /** Phonetic symbol. */
+  phoneticSymbol: string;
+
+  /** Online audio recording file. */
+  audio: WordAudio[];
+
+  /** Collection of word definition along with its sample sentence. */
+  explaination: WordExplaination[];
+
+  /** Explaination of the word's origin. */
+  etymology: string;
+
+  /** When the word is added. */
+  addedAt: Date;
+
+  /** When the word should be studied. */
+  nextDue: Date;
 }
