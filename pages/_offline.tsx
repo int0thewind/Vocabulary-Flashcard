3d6f680c204a0cca93f9e7cee479a994d4daea69
Hanzhi Yin <yinhanzhi@gmail.com>
2021-04-21 15:00:10 -0500

    add offline page and remove sw to git repo

---
+++ pages/_offline.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@material-ui/core';
+import MiddleCenter from 'src/component/MiddleCenter';
+
+function Offline() {
+  return (
+    <MiddleCenter>
+      <Typography variant="h1" color="textPrimary">
+        Offline
+      </Typography>
+      <Typography variant="subtitle1" color="textSecondary">
+        This page was not cached. Please connect to network to see the content.
+      </Typography>
+    </MiddleCenter>
+  );
+}
+
+export default Offline;
